cffbe5d9b76b425f317bacb091795ca2f7592d21
citizen0x <instinctauto@gmail.com>
2021-04-20 16:19:09 +0000

    test update refactoring

--- src/main/java/com/ecom/appSf/entities/RoleEntity.java
+++ src/main/java/com/ecom/appSf/entities/RoleEntity.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 
 @Setter
@@ -22,6 +23,5 @@ public class RoleEntity {
     private String role;
 
     @OneToMany
-    @JoinColumn(name="user_id")
-    private UserEntity user;
+    private List<UserEntity> user ;
 }
--- src/main/java/com/ecom/appSf/entities/UserEntity.java
+++ src/main/java/com/ecom/appSf/entities/UserEntity.java
@@ -38,7 +38,7 @@ public class UserEntity implements Serializable {
 	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
 	private AddressEntity address;
 
-	@ManyToOne(mappedBy = "user", cascade = CascadeType.ALL)
+	@ManyToOne
 	private RoleEntity role;
 
 }
